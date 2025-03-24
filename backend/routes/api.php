d0551fcc3f6ac1f4d42deabe73c03dfda06c9365
BernatDev6 <bernifogi@gmail.com>
2025-03-24 14:11:36 +0100

    add middleware to check the rol of user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckUserRole;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
@@ -11,9 +12,13 @@ Route::post('register', [AuthController::class, 'register']);
 // Rutas protegidas con autenticación JWT
 Route::middleware('jwt.auth')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('/user/{id}', [UserController::class, 'getUser']);
-    Route::patch('/user/{id}/update-name', [UserController::class, 'updateUsername']);
-    Route::patch('/user/{id}/update-image', [UserController::class, 'updateProfileImage']);
-    Route::patch('/user/{id}/update-password', [UserController::class, 'updatePassword']);
-    Route::delete('/user/{id}/delete', [UserController::class, 'deleteUser']);
+
+    // Rutas donde el usuario debe ser dueño de la cuenta o admin
+    Route::middleware(CheckUserRole::class)->group(function () {
+        Route::get('/user/{id}', [UserController::class, 'getUser']);
+        Route::patch('/user/{id}/update-name', [UserController::class, 'updateUsername']);
+        Route::patch('/user/{id}/update-image', [UserController::class, 'updateProfileImage']);
+        Route::patch('/user/{id}/update-password', [UserController::class, 'updatePassword']);
+        Route::delete('/user/{id}/delete', [UserController::class, 'deleteUser']);
+    });
 });
