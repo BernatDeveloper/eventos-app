8a9c03228af9a2e63693bb61e9f0eafc8290ded7
BernatDev6 <bernifogi@gmail.com>
2025-04-10 14:45:13 +0200

    add pagination and name filter in admin user managment, it works but in getAllUsers in AdminController the getCollection() marks error

---
+++ frontend/src/pages/admin/users/components/UserFilter.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { UserFilterProps } from "../../../../types/user";
+
+export const UserFilter: React.FC<UserFilterProps> = ({ onFilterChange }) => {
+  const [filter, setFilter] = useState<string>("");
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newFilter = event.target.value;
+    setFilter(newFilter);
+    onFilterChange(newFilter);
+  };
+
+  return (
+    <div className="mb-4 flex items-center">
+      <input
+        type="text"
+        value={filter}
+        onChange={handleFilterChange}
+        className="px-4 py-2 border border-gray-300 rounded-l-md"
+        placeholder="Filtrar por nombre"
+      />
+    </div>
+  );
+};
