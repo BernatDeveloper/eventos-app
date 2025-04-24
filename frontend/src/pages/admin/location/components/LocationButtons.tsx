8f60183785059cbff01da6224841814df7ff9765
BernatDev6 <bernifogi@gmail.com>
2025-04-24 12:28:26 +0200

    Refeactor LocationModal component

---
+++ frontend/src/pages/admin/location/components/LocationButtons.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { LocationButtonsProps } from '../../../../types/location';
+
+export const LocationButtons: React.FC<LocationButtonsProps> = ({
+    handleSaveLocation,
+    handleDeleteLocation,
+    onClose,
+    mode,
+    isSaveDisabled,
+}) => {
+    return (
+        <div>
+            <button
+                onClick={handleSaveLocation}
+                disabled={isSaveDisabled}
+                className={`mt-4 w-full py-2 rounded text-white transition ${isSaveDisabled ? "bg-blue-300 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"}`}
+            >
+                Save Location
+            </button>
+            {mode === "edit" && (
+                <button
+                    onClick={handleDeleteLocation}
+                    className="mt-2 w-full bg-red-500 text-white py-2 rounded hover:bg-red-600"
+                >
+                    Delete Location
+                </button>
+            )}
+            <button
+                onClick={onClose}
+                className="mt-2 w-full bg-gray-500 text-white py-2 rounded hover:bg-gray-600"
+            >
+                Close
+            </button>
+        </div>
+    );
+};
---
+++ frontend/src/pages/admin/location/components/LocationMap.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvent, useMap } from 'react-leaflet';
+import { LocationMapProps } from '../../../../types/location';
+
+// Function to handle the center of the map
+const SetMapCenter = ({ lat, lng }: { lat: number; lng: number }) => {
+    const map = useMap();
+    map.setView([lat, lng], 15); // Centra el mapa en la nueva ubicación
+    return null;
+};
+
+// Function to handle map click events and update the location state with lat and lng
+const MapClickHandler = ({ setEditedLocation }: any) => {
+    useMapEvent("click", (e) => {
+        const { lat, lng } = e.latlng;
+        setEditedLocation((prevState: any) => ({
+            ...prevState,
+            latitude: lat,
+            longitude: lng,
+        }));
+    });
+    return null;
+};
+
+export const LocationMap: React.FC<LocationMapProps> = ({ latitude, longitude, setEditedLocation }) => {
+    return (
+        <div className="relative h-80 mb-4">
+            <MapContainer center={[latitude, longitude]} zoom={15} style={{ height: "100%", width: "100%" }}>
+                <MapClickHandler setEditedLocation={setEditedLocation} />
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[latitude, longitude]}>
+                    <Popup>{`Lat: ${latitude}, Lng: ${longitude}`}</Popup>
+                </Marker>
+                <SetMapCenter lat={latitude} lng={longitude} />
+            </MapContainer>
+        </div>
+    );
+};
